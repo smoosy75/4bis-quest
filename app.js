8259f3f6335e4d4f6d002f5d9b617a422d135d4e
MUSTAPHA SAID <mustaphasaid.pr@gmail.com>
2022-11-07 11:33:42 +0100

    express 2 done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5007;
+
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -14,6 +17,12 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
 
 app.listen(port, (err) => {
   if (err) {
